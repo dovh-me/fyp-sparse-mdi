cfe66d9dd4ac5b23ac395608ea4084be8120aba3
hosura <h.osura@directfn.com>
2025-04-01 02:30:36 +0530

    MInor changes to the docker files

--- setup_tc.sh
+++ setup_tc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Add latency of 100ms with 10ms jitter
-tc qdisc add dev eth0 root netem delay 100ms 10ms
+tc qdisc add dev eth0 root netem delay 20ms 10ms
 
 # Keep running the main process
 exec "$@"
